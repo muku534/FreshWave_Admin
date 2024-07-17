7817b61a90b7d3d07aabd47e889f47a13227bfc8
muku534 <prajapatimukesh0111@gmail.com>
2024-07-17 14:19:15 +0530

    Implement initial admin dashboard design

---
+++ src/components/menu/page.jsx
@@ -0,0 +1,265 @@
+import Image from "next/image"
+import Link from "next/link"
+import {
+    Home,
+    LineChart,
+    Package,
+    Package2,
+    Search,
+    Settings,
+    ShoppingCart,
+    Users2,
+    CircleUser,
+    Menu,
+
+} from "lucide-react"
+
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+    TooltipProvider,
+} from "@/components/ui/tooltip"
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+
+export default function Navbar() {
+    return (
+        <div className="flex w-full flex-col bg-muted/40">
+            <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+                <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+                    <Link
+                        href="/"
+                        className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+                    >
+                        <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
+                        <span className="sr-only">Acme Inc</span>
+                    </Link>
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="/"
+                                    className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                                >
+                                    <Home className="h-5 w-5" />
+                                    <span className="sr-only">Dashboard</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">Dashboard</TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="/Orders"
+                                    className="flex h-9 w-9 items-center justify-center rounded-lg  text-muted-foreground  transition-colors hover:text-foreground md:h-8 md:w-8"
+                                >
+                                    <ShoppingCart className="h-5 w-5" />
+                                    <span className="sr-only">Orders</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">Orders</TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="/Products"
+                                    className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                                >
+                                    <Package className="h-5 w-5" />
+                                    <span className="sr-only">Products</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">Products</TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="/Customers"
+                                    className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                                >
+                                    <Users2 className="h-5 w-5" />
+                                    <span className="sr-only">Customers</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">Customers</TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="#"
+                                    className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                                >
+                                    <LineChart className="h-5 w-5" />
+                                    <span className="sr-only">Analytics</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">Analytics</TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                </nav>
+                <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="/Settings"
+                                    className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                                >
+                                    <Settings className="h-5 w-5" />
+                                    <span className="sr-only">Settings</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">Settings</TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                </nav>
+            </aside>
+            <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+                <header className="sticky top-0 flex h-14 lg:h-12 lg:pb-3 items-center gap-4 border-b bg-background px-4 md:px-6">
+                    <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+
+                        <Link
+                            href="/"
+                            className="text-foreground transition-colors hover:text-foreground"
+                        >
+                            Dashboard
+                        </Link>
+                        <Link
+                            href="/Orders"
+                            className="text-muted-foreground transition-colors hover:text-foreground"
+                        >
+                            Orders
+                        </Link>
+                        <Link
+                            href="/Products"
+                            className="text-muted-foreground transition-colors hover:text-foreground"
+                        >
+                            Products
+                        </Link>
+                        <Link
+                            href="/Customers"
+                            className="text-muted-foreground transition-colors hover:text-foreground"
+                        >
+                            Customers
+                        </Link>
+                        <Link
+                            href="#"
+                            className="text-muted-foreground transition-colors hover:text-foreground"
+                        >
+                            Analytics
+                        </Link>
+                    </nav>
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <Button
+                                variant="outline"
+                                size="icon"
+                                className="shrink-0 md:hidden"
+                            >
+                                <Menu className="h-5 w-5" />
+                                <span className="sr-only">Toggle navigation menu</span>
+                            </Button>
+                        </SheetTrigger>
+                        <SheetContent side="left">
+                            <nav className="grid gap-6 text-lg font-medium">
+                                <Link
+                                    href="/"
+                                    className="flex items-center gap-2 text-lg font-semibold"
+                                >
+                                    <Package2 className="h-6 w-6" />
+                                    <span className="sr-only">Acme Inc</span>
+                                </Link>
+                                <Link href="/" className="hover:text-foreground">
+                                    Dashboard
+                                </Link>
+                                <Link
+                                    href="/Orders"
+                                    className="text-muted-foreground hover:text-foreground"
+                                >
+                                    Orders
+                                </Link>
+                                <Link
+                                    href="/Products"
+                                    className="text-muted-foreground hover:text-foreground"
+                                >
+                                    Products
+                                </Link>
+                                <Link
+                                    href="/Customers"
+                                    className="text-muted-foreground hover:text-foreground"
+                                >
+                                    Customers
+                                </Link>
+                                <Link
+                                    href="#"
+                                    className="text-muted-foreground hover:text-foreground"
+                                >
+                                    Analytics
+                                </Link>
+                            </nav>
+                        </SheetContent>
+                    </Sheet>
+                    <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+                        <form className="ml-auto flex-1 sm:flex-initial">
+                            <div className="relative">
+                                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                                <Input
+                                    type="search"
+                                    placeholder="Search products..."
+                                    className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+                                />
+                            </div>
+                        </form>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="secondary" size="icon" className="rounded-full">
+                                    <CircleUser className="h-5 w-5" />
+                                    <span className="sr-only">Toggle user menu</span>
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>Settings</DropdownMenuItem>
+                                <DropdownMenuItem>Support</DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>Logout</DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
+                </header>
+            </div>
+        </div>
+    )
+}
