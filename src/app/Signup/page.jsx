b0076a3db091cd659d9c70082542ce617ef165d1
muku534 <prajapatimukesh0111@gmail.com>
2024-07-18 15:30:06 +0530

    initialize Firebase and add email/password signup functionality

--- src/app/Signup/page.jsx
+++ src/app/Signup/page.jsx
@@ -1,5 +1,7 @@
-import Link from "next/link"
+"use client"
 
+import React, { useState, useEffect } from "react"
+import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import {
     Card,
@@ -10,10 +12,35 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../firebase"
+import { collection, doc, setDoc } from "firebase/firestore"
 
 export default function Signup() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSignUp = async () => {
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+
+            await setDoc(doc(db, collection, user.uid), {
+                firstName,
+                email,
+                password,
+                role
+            });
+
+        } catch (error) {
+            setError(error.message);
+        }
+    }
+
     return (
-        <div>
+        <div className="mt-28">
             <Card className="mx-auto max-w-sm">
                 <CardHeader>
                     <CardTitle className="text-xl">Sign Up</CardTitle>
@@ -26,11 +53,13 @@ export default function Signup() {
                         <div className="grid grid-cols-2 gap-4">
                             <div className="grid gap-2">
                                 <Label htmlFor="first-name">First name</Label>
-                                <Input id="first-name" placeholder="Max" required />
+                                <Input id="first-name" placeholder="Max" value={firstName}
+                                    onChange={(e) => setFirstName(e.target.value)} required />
                             </div>
                             <div className="grid gap-2">
                                 <Label htmlFor="last-name">Last name</Label>
-                                <Input id="last-name" placeholder="Robinson" required />
+                                <Input id="last-name" placeholder="Robinson" value={lastName}
+                                    onChange={(e) => setLastName(e.target.value)} required />
                             </div>
                         </div>
                         <div className="grid gap-2">
@@ -39,14 +68,17 @@ export default function Signup() {
                                 id="email"
                                 type="email"
                                 placeholder="m@example.com"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                                 required
                             />
                         </div>
                         <div className="grid gap-2">
                             <Label htmlFor="password">Password</Label>
-                            <Input id="password" type="password" />
+                            <Input id="password" type="password" value={password}
+                                onChange={(e) => setPassword(e.target.value)} />
                         </div>
-                        <Button type="submit" className="w-full">
+                        <Button type="submit" className="w-full" onPress={handleSignUp}>
                             Create an account
                         </Button>
                         <Button variant="outline" className="w-full">
